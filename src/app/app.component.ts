ed65e67971a469d24b4f74681782499cba1e8d08
Lan Allanigue <allanallanigue@gmail.com>
2020-02-11 18:09:57 +0800

    new slider instance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngslider';
+  public imagesUrl;
+
+  ngOnInit() {
+    this.imagesUrl = ['assets/images/image1.jpg',
+    'assets/images/image2.jpg',
+    'assets/images/image3.jpg',
+    'assets/images/image4.jpg',
+    'assets/images/image5.jpg',
+    'assets/images/image6.jpg',
+    'assets/images/image7.jpg',
+    'assets/images/image8.jpg',
+    'assets/images/image9.jpg'];
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { SliderModule } from 'angular-image-slider';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,6 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    SliderModule,
     AppRoutingModule
   ],
   providers: [],
