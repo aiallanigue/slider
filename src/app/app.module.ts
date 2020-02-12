774385f84390bcc7b8aa91f1e90447f55af54794
Lan Allanigue <allanallanigue@gmail.com>
2020-02-12 12:57:32 +0800

    new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { SliderModule } from 'angular-image-slider';
 import { AppComponent } from './app.component';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    MatIconModule,
+    MatMenuModule,
+    MatSliderModule,
     BrowserModule,
     BrowserAnimationsModule,
     SliderModule,
